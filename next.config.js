1768dcf48aca48d75a875a7f502c5f83cf07306b
ycnugroho” <5026231032@student.its.ac.id>
2026-06-10 12:08:20 +0700

    fix: remove standalone and transpile packages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: ['192.168.56.1'],
-  output: 'standalone',
-  transpilePackages: ['iron-session'],
 };
 
 module.exports = nextConfig;

957bc9bd37809f00fcaca880d7bebbf03bba93d9
ycnugroho” <5026231032@student.its.ac.id>
2026-06-18 17:35:53 +0700

    disable turbopack

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   allowedDevOrigins: ['192.168.56.1'],
   serverExternalPackages: ['@libsql/client', 'bcryptjs', 'iron-session'],
   output: 'standalone',
+  experimental: {
+    turbopack: false,
+  },
 };
 
 module.exports = nextConfig;
